ef672b28836c6f1489daa7f650d734f9451b34a7
AyishathNahda <ayishathnahda.9i@gmail.com>
2025-01-29 10:08:07 +0530

    Create schema.js
    added schema.js

---
+++ schema.js
@@ -0,0 +1,46 @@
+const mongoose=require('mongoose');
+
+const schemaUser=new mongoose.Schema({
+  user_fields:{
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    email:{
+      type: String,
+      required: true,
+      unique: true
+    },
+    password:{
+      type: String,
+      required: true
+    },
+    roles:{
+      type:[String]
+    }
+  },
+
+  profile_fields: {
+    firstname:{
+      type: String
+    },
+    lastname:{
+      type: String
+    },
+    age:{
+      type: Number
+    }
+  },
+
+  activity_tracking:{
+    last_login: {
+      type: Date
+    }
+  }
+})
+
+module.exports =mongoose.model('User', schemaUser);
+    
+    
+      
